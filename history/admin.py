97e28c8688a6f124791261a67566fd91d18a5122
Andrii Tomalia <logotip123@yahoo.com>
2020-02-11 17:58:23 +0200

    add models.py

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ExchangeRateHistory, Currency
+
+
+admin.site.register(ExchangeRateHistory)
+admin.site.register(Currency)
--- history/models.py
+++ history/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    currency = models.CharField(max_length=5, unique=True)
+
+    def __str__(self):
+        return self.currency
+
+
+class ExchangeRateHistory(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    purchase_rate = models.DecimalField(max_digits=6, decimal_places=2)
+    selling_rate = models.DecimalField(max_digits=6, decimal_places=2)
+    valid_from = models.DateField()
+    valid_until = models.DateField(blank=True, null=True)
+
+    class Meta:
+        unique_together = (('currency', 'valid_from'), ('currency', 'valid_until'))
+
+    def __str__(self):
+        return f"{self.currency}: {self.valid_from} - {self.valid_until}"
